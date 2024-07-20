2a93cdca78adf7449285a264bafb84ce8863bff3
John Christensen <johnawesomejr@gmail.com>
2024-07-20 11:18:35 -0500

    more

--- sample.env.php
+++ sample.env.php
@@ -1,8 +1,7 @@
 <?php
+$GLOBAL_photographerName = "";
 $GLOBAL_emailSender = "";
 $GLOBAL_googleAppPassword = ""; #Google App Password (no spaces)
 $GLOBAL_phoneNumber = "";
-
-$GLOBAL_emailSubjectLine = "";
-
-
+$GLOBAL_pricePerPhoto = "15";
+$GLOBAL_emailSubjectLine = "Somone bought your photos!";
